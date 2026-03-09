9ada50acb8d4e2542c0b794243ca782c28cbbb20
Pedro Henrique Lourenço Pierani <130299627+phlpierani@users.noreply.github.com>
2026-03-08 23:20:57 -0300

    Add files via upload

---
+++ codetickets/src/test/java/br/com/alura/demo/CodeticketsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeticketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
