dace49af5354ed0830b621db2d2f5a30c80a4d64
phlpierani <phl.pierani@gmail.com>
2026-03-14 01:23:27 -0300

    Add Importacao entity and batch job configuration for ticket processing

---
+++ codetickets/src/main/java/br/com/alura/demo/ImportacaoJobConfig.java
@@ -0,0 +1,62 @@
+package br.com.alura.demo;
+
+import org.springframework.batch.core.job.Job;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.parameters.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.Step;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.infrastructure.item.ItemReader;
+import org.springframework.batch.infrastructure.item.ItemWriter;
+import org.springframework.batch.infrastructure.item.file.FlatFileItemReader;
+import org.springframework.batch.infrastructure.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.io.Reader;
+
+@Configuration
+public class ImportacaoJobConfig {
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    @Bean
+    public Job job(Step passoInicial, JobRepository jobRepository) {
+        return new JobBuilder("geracao-tickets", jobRepository )
+                .start(passoInicial)
+                .incrementer(new RunIdIncrementer())
+                .build();
+        // O RunIdIncrementer é usado para garantir que cada execução do job tenha um ID único,
+        // permitindo que o mesmo job seja executado várias vezes sem conflitos de ID.
+    }
+
+    @Bean
+    public Step passoInicial(ItemReader<Importacao> reader,
+                             ItemWriter<Importacao> writer,
+                             JobRepository jobRepository) {
+
+        return new StepBuilder("passo-inicial", jobRepository)
+                .<Importacao, Importacao>chunk(100) // Define o tamanho do chunk para processamento em lotes
+                .reader(reader) // Define o leitor de itens, que é responsável por ler os dados de origem (por exemplo, um arquivo CSV ou um banco de dados)
+                .writer(writer) // Define o escritor de itens, que é responsável por escrever os dados processados para o destino (por exemplo, um banco de dados)
+                .transactionManager(transactionManager) // Define o gerenciador de transações para garantir que as operações de leitura e escrita sejam realizadas
+                .build();
+    }
+
+    @Bean
+    public ItemReader<Importacao> reader() {
+        return new FlatFileItemReaderBuilder<Importacao>()
+                .name("leitura-csv") // Define o nome do leitor de itens, que pode ser usado para fins de identificação e depuração
+                .resource(new FileSystemResource("files/dados.csv")) // Especifica o recurso de entrada, que neste caso é um arquivo CSV localizado no sistema de arquivos
+                .comments("--") // Define o prefixo para linhas de comentário no arquivo CSV, que serão ignoradas durante a leitura
+                .delimited() // Especifica que o arquivo é delimitado (por exemplo,
+                .names("cpf", "cliente", "nascimento", "evento", "data", "tipoIngresso", "valor") // Define os nomes das colunas no arquivo CSV, que serão mapeados para os campos da classe Importacao
+                .targetType(Importacao.class) // Especifica a classe de destino para o mapeamento dos dados lidos do arquivo CSV, permitindo que os dados sejam convertidos em objetos do tipo Importacao
+                .build();
+    }
+}
+
